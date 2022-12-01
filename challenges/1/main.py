b63b36f47f5f2998ae5db8ec6015ecc5356c3a42
Mattie Nash <mnash@kittymail.com>
2022-12-01 12:03:21 -0600

    Optimized day 1

--- challenges/1/main.py
+++ challenges/1/main.py
@@ -1,13 +1,4 @@
 with open("input.txt","r") as input:
-    inputStr = input.read()
-    elves = inputStr.split("\n\n")
-    elveTotals = []
-    for elve in elves:
-        elveLines = elve.split("\n")
-        elveTotal = 0
-        for line in elveLines:
-            elveTotal += int(line)
-        elveTotals.append(elveTotal)
-    elveTotals.sort(reverse=True)
+    elveTotals = sorted([sum([int(food) for food in elve.split("\n")]) for elve in input.read().split("\n\n")], reverse=True)
     print(elveTotals[0])
-    print(elveTotals[0]+elveTotals[1]+elveTotals[2])
+    print(sum(elveTotals[0:3]))
